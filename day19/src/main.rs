57dae4ff71126994e3a1067803fc5cf6690e22d2
Joni Lehmuksela <10558717+Walop@users.noreply.github.com>
2020-12-19 20:29:38 +0200

    Day 19, after finding the problem just cheat and hard code

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -81,16 +81,64 @@ fn main() {
 
     println!("Valid count: {}", valid_count);
 
+    let (rule8_len,_) = resolve_rules(messages[0], &rules, 0, 8, 0);
+
     // NOT WORKING AFTER THIS
-    rules.entry(8).and_modify(|r| *r = Rule {match_char: None, refs: Some(vec![42]), refs2: Some(vec![42, 8])});
-    rules.entry(11).and_modify(|r| *r = Rule {match_char: None, refs: Some(vec![42,31]), refs2: Some(vec![42, 11, 31])});
+    rules.entry(8).and_modify(|r| *r = Rule {match_char: None, refs: Some(vec![42, 8]), refs2: Some(vec![42])});
+    rules.entry(11).and_modify(|r| *r = Rule {match_char: None, refs: Some(vec![42, 31]), refs2: Some(vec![42, 11, 31])});
 
     valid_count = 0;
 
     for message in messages.iter() {
-        let (offset, result) = resolve_rules(message, &rules, 0, 0, 0);
+        let mut offset1 = 0;
+        let mut result1 = true;
+        while result1 {
+            let (offset, result) = resolve_rules(message, &rules, offset1, 42, 0);
+            result1 = result;
+            if result {
+                offset1 += offset;
+            }
+        }
+
+        if offset1 == 0 {
+            continue;
+        }
+
+        let mut offset2 = offset1;
+        let mut result2 = false;
+        let mut offset3 = 0;
+
+        while !result2 && offset2 >= rule8_len {
+            let mut result3 = true;
+            let mut offset4 = offset2;
+            let mut result_count1 = 0;
+            while result3 {
+                let (offset, result) = resolve_rules(message, &rules, offset4, 42, 0);
+                result3 = result;
+                if result {
+                    result_count1 += 1;
+                    offset4 += offset;
+                }
+            }
+            let mut result4 = true;
+            let mut offset5 = offset4;
+            let mut result_count2 = 0;
+            while result4 {
+                let (offset, result) = resolve_rules(message, &rules, offset5, 31, 0);
+                result4 = result;
+                if result {
+                    result_count2 += 1;
+                    offset5 += offset;
+                }
+            }
+            offset3 = offset5;
+            result2 = result_count1 > 0 && result_count1 == result_count2;
+            if !result2 {
+                offset2 -= rule8_len;
+            }
+        }
         //println!("Final offset {}, result {}", offset, result);
-        if result && offset == message.len() {
+        if result2 && offset3 == message.len() {
             println!("{}", message);
             valid_count += 1;
         }
@@ -100,15 +148,18 @@ fn main() {
 }
 
 fn resolve_rules(message: &str, rules: &std::collections::HashMap<i32, Rule>, char_idx: usize, rule_nmb: i32, rec: usize) -> (usize, bool) {
-    print!("Rule {} -> ", rule_nmb);
+    //print!("Rule {} -> ", rule_nmb);
+    if rule_nmb == 11 {
+        //println!("--Evaluating rule 11 level {}", rec);
+    }
     if let Some(match_char) = rules[&rule_nmb].match_char {
         match message.chars().nth(char_idx) {
             Some(c) => {
-                println!("Matching {} with {} at index {}: {}", match_char, c, char_idx, match_char == c);
+                //println!("Matching {} with {} at index {}: {}", match_char, c, char_idx, match_char == c);
                 return (1, match_char == c);
             },
             None => {
-                println!("Ran out of message at index {}", char_idx);
+                //println!("Ran out of message at index {}", char_idx);
                 return (0, false);
             }
         }
@@ -126,8 +177,8 @@ fn resolve_rules(message: &str, rules: &std::collections::HashMap<i32, Rule>, ch
             char_offset += offset;
         }
         if final_result {
-            println!("First matched, short circuit");
-            println!("Rec {} Rule {} true", rec, rule_nmb);
+            //println!("First matched, short circuit");
+            //println!("Rec {} Rule {} true at {} next offset {}", rec, rule_nmb, char_idx, char_offset);
             return (char_offset, final_result);
         }
         if let Some(refs2) = &rules[&rule_nmb].refs2 {
@@ -135,13 +186,16 @@ fn resolve_rules(message: &str, rules: &std::collections::HashMap<i32, Rule>, ch
             for rule_ref in refs2 {
                 let (offset, result) = resolve_rules(&message, rules, char_idx + char_offset, *rule_ref, rec+1);
                 if !result {
-                    return (char_offset, false);
+                    return (0, false);
                 }
                 char_offset += offset;
             }
-            println!("Second matched");
-            println!("Rule {} true", rule_nmb);
+            //println!("Second matched");
+            //println!("Rec {} Rule {} true at {} next offset {}", rec, rule_nmb, char_idx, char_offset);
             return (char_offset, true);
+        }
+        if rule_nmb == 31 {
+            //println!("Rule 31 did not match");
         }
         return (0, false);
     }
